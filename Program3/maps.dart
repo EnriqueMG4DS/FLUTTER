5d12281238383b8f00bca705f468d2c694c41ebe
Enrique Morales <enriquemorales6683@gmail.com>
2025-09-09 23:35:17 -0600

    ejemplos de uso de maps, version no tan usada

---
+++ Program3/maps.dart
@@ -0,0 +1,22 @@
+void main(){
+final Map<String, dynamic> pokemon={
+  'name': 'Ditto',
+  'hp': 100,
+  'isAlive': true,
+  'abilities': <String>['impostor'],
+  'sprites': <int,String>{
+    1:'ditto/front.png',
+    2:'ditto/back.png'
+  }
+};
+
+print(pokemon);
+
+print('Name: ${pokemon['name']}');
+print('Sprite: ${pokemon['sprites']}');
+
+print('back: ${pokemon['sprites'][2]}');
+print('front: ${pokemon['sprites'][1]}');
+
+
+}
