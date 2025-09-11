02108c7fcb46a0a324222fc12a0731d99ed5983e
Enrique Morales <enriquemorales6683@gmail.com>
2025-09-11 14:06:05 -0600

    Uso de requerimientos en las funciones

--- Program5/funciones.dart
+++ Program5/funciones.dart
@@ -7,6 +7,8 @@ print(GreetEveryOne());// la forma de llamar una funcion sigue siendo la misma
 print('Suma: ${addTwoNumbers(10, 20)}');
 
 print('resta: ${AddTownNumber(20, 10)}');
+
+print(greetPerson(name: 'kikee',message: 'holaaa' ));
 }
 
 
@@ -23,9 +25,14 @@ int addTwoNumbers(int a,int b){ //para que la funcion pueda trabajar en este cas
 
 int AddTownNumber(int a,int b)=> a-b; // solo funciona para funciones sencillas
 
-int addTwoNumbersOptional(int a,[int? b]){ // con los [] se determina que su valor no es obligatorio
+int addTwoNumbersOptional(int a,[int? b, int c=0]){ // con los [] se determina que su valor no es obligatorio
 
   b = b ?? 0; // en caso de ser nulo este pasa directamente el valor de 0
   return a + b ;
 
+}
+// AL USAR REQUIRED VULVE A SER UNA VARIABLE FORSOSA DE USARR PARA TRABAJAR SOBRE LA FUCNION
+String greetPerson({ required name, String message='Hola'}){ // al trabajarlos entre llaves se vuelven variables opcionales 
+  
+  return '$message, $name';
 }
