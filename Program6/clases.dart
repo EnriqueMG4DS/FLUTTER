88fdef9add5179943a92587a6ae84954182e9c3c
Enrique Morales <enriquemorales6683@gmail.com>
2025-09-11 19:59:04 -0600

    Actualizacion del uso de clases de como ingresar datos y observar comportamientos de las variables segun ingresadas

--- Program6/clases.dart
+++ Program6/clases.dart
@@ -1,8 +1,8 @@
 void main(){
   //final wolverine = new hero('logan','Regeneracion');
-  final hero wolverine = hero('logan','Regeneracion'); //NO ES NECESARIO USAR EL NEW
-
-  print(wolverine);
+  //final hero wolverine = hero('logan','Regeneracion'); //NO ES NECESARIO USAR EL NEW
+final hero wolverine = hero(name: 'Logan',power: 'regeneration');
+  print(wolverine.toString());
   print(wolverine.name);
   print(wolverine.power);
 }
@@ -20,10 +20,20 @@ class hero{
 
 //SEGUNDA FORMA DE INGRESAR DATOS A LA CLASE
 
-hero(this.name,this.power);
+//hero(this.name,this.power);
+
+hero({
+  required this.name,
+  this.power='sin poder'
+});
+
 
     //Para almacenar estos datos tienen que estar entre llaves
     //{name=pName; //Para almacenar un valor sobre el objeto
     //this.power=pPower;} // Almacenar otro valor al mismo objeto
-  
+  @override// es necesario para spbreescribir informacion NO ES OBLIGATORIO
+  String toString(){
+    return '$name - $power'; //una forma de contener la informacion de la clase para ser mostrada 
+  }
 }
+
