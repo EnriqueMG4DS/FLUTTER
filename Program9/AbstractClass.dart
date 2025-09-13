ae92f19b1e0843f1a0b00bc1d2f6d9ec17d377db
Enrique Morales <enriquemorales6683@gmail.com>
2025-09-12 20:49:06 -0600

    integracion de clases con extends y como manipular su informacion y funciones

---
+++ Program9/AbstractClass.dart
@@ -0,0 +1,45 @@
+
+enum  plantType{nuclear, wind, water} // se definen los tipos de entradas
+
+abstract class EnergyPlant{
+
+  double enrgyLeft; //los es una unidad de consumo para una futura funcion 
+  plantType type; // nuclear, wind, water
+
+  EnergyPlant({// esto se requiere para de cierto modo inicializar las variables
+    required this.enrgyLeft,
+    required this.type 
+  });
+
+  void ConsumeEnergy(double amount);
+  //{
+  //  throw UnimplementedError();
+  //}
+}
+
+// extends o implements
+class windPlant extends EnergyPlant{ //forma de eredar una clase
+
+  windPlant({required double initialEnergy})
+  : super(enrgyLeft: initialEnergy, type: plantType.wind);
+
+  @override
+  void ConsumeEnergy(double amount){
+    enrgyLeft-=amount;
+  }
+}
+
+void main(){
+  final windPlantt = windPlant(initialEnergy:100); //checar que las variables no choquen entre si
+
+  print(windPlantt);
+
+  print('wind:${charcheFound(windPlantt)}');
+}
+
+double charcheFound(EnergyPlant plant){
+  if(plant.enrgyLeft<10){
+    throw Exception('No enough energy');
+  }
+  return plant.enrgyLeft-10;
+}
