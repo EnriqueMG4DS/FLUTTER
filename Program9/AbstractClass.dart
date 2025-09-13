445f87cd04adc109aa877ef0406b3fd77420ec2d
Enrique Morales <enriquemorales6683@gmail.com>
2025-09-12 22:17:16 -0600

    mas de polimorfismo

--- Program9/AbstractClass.dart
+++ Program9/AbstractClass.dart
@@ -4,7 +4,7 @@ enum  plantType{nuclear, wind, water} // se definen los tipos de entradas
 abstract class EnergyPlant{
 
   double enrgyLeft; //los es una unidad de consumo para una futura funcion 
-  plantType type; // nuclear, wind, water
+  final plantType type; // nuclear, wind, water
 
   EnergyPlant({// esto se requiere para de cierto modo inicializar las variables
     required this.enrgyLeft,
@@ -31,10 +31,12 @@ class windPlant extends EnergyPlant{ //forma de eredar una clase
 
 void main(){
   final windPlantt = windPlant(initialEnergy:100); //checar que las variables no choquen entre si
+  final nuclearPlant= NuclearPlant(enrgyLeft: 1000);
 
   print(windPlantt);
 
   print('wind:${charcheFound(windPlantt)}');
+  print('Nuclear: ${charcheFound(nuclearPlant)}');
 }
 
 double charcheFound(EnergyPlant plant){
@@ -42,4 +44,19 @@ double charcheFound(EnergyPlant plant){
     throw Exception('No enough energy');
   }
   return plant.enrgyLeft-10;
+}
+
+class NuclearPlant implements EnergyPlant{
+  @override
+  double enrgyLeft;
+  @override
+  final plantType type = plantType.nuclear;
+  
+  NuclearPlant({
+    required this.enrgyLeft
+  });
+  @override
+  void ConsumeEnergy(double amount){
+    enrgyLeft-=(amount*.5);
+  }
 }
