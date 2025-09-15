cf9c756500ad0e623b70564ba724c358934234ed
Enrique Morales <enriquemorales6683@gmail.com>
2025-09-14 22:06:56 -0600

    Uso de widgets y como eliminar el baner de debugging

--- ProgramFlut/hello_world_app/lib/main.dart
+++ ProgramFlut/hello_world_app/lib/main.dart
@@ -6,10 +6,16 @@ void main(){
 }
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key}); //un contructor para que no maque este error
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(child: Text('Hola mundo'))
+    return const MaterialApp( //derivado a que no cambiara este valor en ningun punto de su construccion
+      debugShowCheckedModeBanner: false, //Es para que no aparezca la alerta de que esta en modo debuging
+      home: Scaffold(// el Scaffols se usa para para centralizar pantallas como la funcion DIV en HTML
+        body: Center(child: Text('Hola mundo')),
+
+      )
     );
   }
 
