b22ed070f814a3d10cdc3efa9ba4c006addde9d9
Enrique Morales <enriquemorales6683@gmail.com>
2025-09-16 16:03:03 -0600

    edicion de diseno material ademas de uso de paleta de colores integrada

--- ProgramFlut/hello_world_app/lib/presentacion/screens/counters/counter_screen.dart
+++ ProgramFlut/hello_world_app/lib/presentacion/screens/counters/counter_screen.dart
@@ -5,12 +5,15 @@ class CounterScreen extends StatelessWidget {
   @override // SE GENERA UN CONTENEDOR BACIO 
   Widget build(BuildContext context) {
     return Scaffold(// el Scaffols se usa para para centralizar pantallas como la funcion DIV en HTML
+        appBar: AppBar(
+          title: const Text('Couner Screen'),
+        ),
         body: //Center(child: Text('Counter Screen')),
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center, // Se centra son este comando
               children: const [
-                Text('10', style:TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
+                Text('100', style:TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
                 Text('Clicks',style:TextStyle(fontSize: 25))
               ],
             ),
