0ab0652ba76e8fbd9b68cc8ba4fdfa9299488e1a
Enrique Morales <enriquemorales6683@gmail.com>
2025-09-19 20:19:20 -0600

    Personalizacion de los temas de los widgets

---
+++ Program2Flut/yes_no_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF5C11D4); //COLOR PERSONALIZADO EN UN VARIABLE
+
+const List<Color> _colorThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+];
+
+class AppTheme{
+  final int selectedColor;
+
+  AppTheme({
+     this.selectedColor =0
+    }):assert (selectedColor >=0 && selectedColor <= _colorThemes.length-1,'Colors must be between 0 and ${_colorThemes.length} ');
+
+
+  ThemeData theme(){ //Es un parametro necesario para hacer la conexion con el main
+    return ThemeData(
+      useMaterial3: true, //Estilos que estan siendo implementados
+      colorSchemeSeed: _colorThemes[selectedColor],// representa como el backgraund de la interfaz en este caso toma el de una funcion
+      brightness: Brightness.dark
+    );
+      
+  }
+}
